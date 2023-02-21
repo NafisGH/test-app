4ba794a9f348b4e66ef63cd7497fd37928337963
Nafis Garifullin <nafis-86@inbox.ru>
2023-02-21 19:46:18 +0500

    скрипт создания карточки

--- scripts/create-card/index.js
+++ scripts/create-card/index.js
@@ -1,4 +1,25 @@
 const tmplNewCard = document.querySelector('#tmpl-new-card');
-const newCard = tmplNewCard.content.querySelector('.card').cloneNode(true);
 
-console.log(newCard)
+const listCards = document.querySelector('.list-card')
+
+
+
+const handleCreateNewCard = (title, author, srcImg, dateCreate) => {
+    const newCard = tmplNewCard.content.querySelector('.card').cloneNode(true);
+
+    const headerCard = newCard.querySelector('.header-card');
+    const authorCard = newCard.querySelector('.author-card');
+    const imgCard = newCard.querySelector('.img-card');
+    const cardCreationDate = newCard.querySelector('.card-creation-date');
+
+    headerCard.textContent = title;
+    authorCard.textContent = author;
+    imgCard.src = srcImg;
+    cardCreationDate.textContent = dateCreate;
+
+    return newCard;
+}
+
+const newCard = handleCreateNewCard('Test 1', 'Jon P.', 'https://plus.unsplash.com/premium_photo-1676064229122-ab0d1385efbb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60', '21.02.2023')
+
+listCards.append(newCard)
